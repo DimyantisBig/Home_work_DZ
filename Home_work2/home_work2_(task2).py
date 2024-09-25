c6437b1691d00fcc9888aa4cbb6c6706dcb0cc5c
Dmitriy Zaitsev <dimyantis@gmail.com>
2024-09-25 12:25:51 -0700

    New direct

---
+++ Home_work2/home_work2_(task2).py
@@ -0,0 +1,14 @@
+#Hello world
+print('Hello world')
+#Hello-world
+print('Hello','world',sep='-')
+#Hello world!!!
+print('Hello world',end='!!!\n')
+#Hello-world!!! 
+print('Hello','world',sep='-',end='!!!\n')
+#print  1 2 3 4 5
+print(1,2,3,4,5)
+#print Hello world! and three empty lines down
+print('Hello world!',end='\n.\n.\n.')
+#Print Hello world! from a new line and indent and print I am winner!
+print('Hello World!',end='\n\n\t\t I am winner!')
---
+++ Home_work2/home_work2_(task3).py
@@ -0,0 +1,4 @@
+print('#####\n#\t#\n#\t#\n#####')
+print(' ')
+print('#\t#\n#\t#\n#####\n#\t#\n#\t#')
+print(' ')
