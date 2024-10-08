148ca316d3253ca3b489a7aeb276cfdd9c138ea7
Dmitriy Zaitsev <dimyantis@gmail.com>
2024-10-08 05:33:26 -0700

    Add my new file: task 1,2,3,4

--- Home_work4/task_2.py
+++ Home_work4/task_2.py
@@ -1,6 +1,6 @@
 num = input("Enter a number: ")
-if len(num) > 10:
-    print('Your number is too long')
+if len(num) != 10:
+    print('Your number is wrong')
 elif num.isdigit():
     print('Good number, thank you')
 else:
