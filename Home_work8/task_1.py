9f4042e26327fa8476cf67e1d996e57e0535204e
Dmitriy Zaitsev <dimyantis@gmail.com>
2024-10-20 16:51:53 -0700

    Change file task3

--- Home_work8/task_1.py
+++ Home_work8/task_1.py
@@ -1,4 +1,5 @@
 def favorite_movi(name): # Создаем функцию
     print('My favorite movi is named', name ,'!') # Что будет делать функция
 
+
 favorite_movi('Брийльянтовая рука') # Вызов нашей функции
